2ec67b387a4345c6f09657729fe1aebe14b18a1e
James Stine <Leon.Blade@gmail.com>
2017-02-07 05:54:44 -0500

    Change XnbError slightly.

--- xnb/XnbError.js
+++ xnb/XnbError.js
@@ -3,9 +3,9 @@ const chalk = require('chalk');
 class XnbError extends Error {
     constructor(message = '') {
         super(message);
-        this.name = chalk.bold.red('[ERROR] ') + this.constructor.name;
+        this.name = chalk.bold.red('[EXCEPTION] ') + this.constructor.name;
         this.message = message;
-        Error.captureStackTrace(this, this.constructor);
+        Error.captureStackTrace(this, XnbError);
     }
 }
 
