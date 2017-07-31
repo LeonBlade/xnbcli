d7903536fd791bf248624cdf4eeffed46960e996
James Stine <james@connectai.com>
2017-07-31 08:38:27 -0400

    For bundling purposes with zeit/pkg

--- app/Xnb/Readers/index.js
+++ app/Xnb/Readers/index.js
@@ -24,5 +24,29 @@ const readers = [
 ];
 
 // loop over readers to export them
-for (let reader of readers)
-    exports[reader] = require(`./${reader}`);
+//for (let reader of readers)
+//    exports[reader] = require(`./${reader}`);
+
+module.exports = {
+    ArrayReader: require('./ArrayReader'),
+    BaseReader: require('./BaseReader'),
+    BmFontReader: require('./BmFontReader'),
+    BooleanReader: require('./BooleanReader'),
+    CharReader: require('./CharReader'),
+    DictionaryReader: require('./DictionaryReader'),
+    DoubleReader: require('./DoubleReader'),
+    EffectReader: require('./EffectReader'),
+    Int32Reader: require('./Int32Reader'),
+    ListReader: require('./ListReader'),
+    NullableReader: require('./NullableReader'),
+    RectangleReader: require('./RectangleReader'),
+    SingleReader: require('./SingleReader'),
+    SpriteFontReader: require('./SpriteFontReader'),
+    StringReader: require('./StringReader'),
+    TBinReader: require('./TBinReader'),
+    Texture2DReader: require('./Texture2DReader'),
+    UInt32Reader: require('./UInt32Reader'),
+    Vector2Reader: require('./Vector2Reader'),
+    Vector3Reader: require('./Vector3Reader'),
+    Vector4Reader: require('./Vector4Reader')
+};
