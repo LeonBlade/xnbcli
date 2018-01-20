4e57d41d6bb2d49c8ab466294ff603c471b07f43
James Stine <leon.blade@gmail.com>
2018-01-20 10:29:34 -0500

    Adding write and get index for reader resolver to verify readers and write content to xnb

--- app/Xnb/ReaderResolver.js
+++ app/Xnb/ReaderResolver.js
@@ -11,12 +11,12 @@ class ReaderResolver {
     /**
      * Creating a new instance of ReaderResolver
      * @constructor
-     * @param {TypeReader[]} readers Array of TypeReaders
+     * @param {BaseReader[]} readers Array of BaseReaders
      */
     constructor(readers) {
         /**
-         * Array of type readers
-         * @type {TypeReader[]}
+         * Array of base readers
+         * @type {BaseReader[]}
          */
         this.readers = readers;
     }
@@ -34,6 +34,26 @@ class ReaderResolver {
             throw new XnbError(`Invalid reader index ${index}`);
         // read the buffer using the selected reader
         return this.readers[index].read(buffer, this);
+    }
+
+    /**
+     * Writes the XNB file contents
+     * @param {BufferWriter} buffer
+     * @param {Object} content 
+     */
+    write(buffer, content) {
+        this.readers[0].write(buffer, content, this);
+    }
+
+    /**
+     * Returns the index of the reader
+     * @param {BaseReader} reader 
+     * @param {Number}
+     */
+    getIndex(reader) {
+        for (let i in this.readers)
+            if (reader.constructor == this.readers[i].constructor)
+                return i;
     }
 }
 
