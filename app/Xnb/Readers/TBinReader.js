00a1f88eeb6936fd9f018f9a6ee7c1d2da58460d
James Stine <leon.blade@gmail.com>
2018-01-21 13:39:19 -0500

    Add map writer.

--- app/Xnb/Readers/TBinReader.js
+++ app/Xnb/Readers/TBinReader.js
@@ -1,5 +1,6 @@
 const BaseReader = require('./BaseReader');
 const BufferReader = require('../../BufferReader');
+const BufferWrite = require('../../BufferWriter');
 const Int32Reader = require('./Int32Reader');
 
 /**
@@ -18,6 +19,13 @@ class TBinReader extends BaseReader {
 
         // return the data
         return { export: { type: this.type, data } };
+    }
+
+    write(buffer, content, resolver) {
+        this.writeIndex(buffer, resolver);
+        const int32Reader = new Int32Reader();
+        int32Reader.write(buffer, content.data.length, null);
+        buffer.concat(content.data);
     }
 
     isValueType() {
