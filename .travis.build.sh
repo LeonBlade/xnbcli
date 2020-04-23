aeeb3183414229bc1f72c8e9cb877a07c862ac58
James Stine <leon.blade@gmail.com>
2020-04-23 12:57:16 -0400

    Fix up the deployment

--- .travis.build.sh
+++ .travis.build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Copy the node binaries into the main folder
-cp node_modules/dxt/build/Release/dxt.node .
 cp node_modules/lz4/build/Release/xxhash.node .
 cp node_modules/lz4/build/Release/lz4.node .
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     pkg xnbcli.js --targets macos
-    zip xnbcli-macos.zip xnbcli dxt.node xxhash.node lz4.node packed unpacked pack.command unpack.command
+    zip xnbcli-macos.zip xnbcli xxhash.node lz4.node packed unpacked pack.command unpack.command
 else
     pkg xnbcli.js --targets linux
-    zip xnbcli-linux.zip xnbcli dxt.node xxhash.node lz4.node packed unpacked pack.sh unpack.sh
+    zip xnbcli-linux.zip xnbcli xxhash.node lz4.node packed unpacked pack.sh unpack.sh
 fi
